9dc532d503884a5645e6c36abb8a652510e924c7
Rajat Singh <105720162+rjrajat00@users.noreply.github.com>
2022-08-25 20:57:18 +0530

    Updates

---
+++ precache-manifest.7b069aea1f9486f5ae8b1ae2733c0208.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e38d0cf372aea3d9fb71",
+    "url": "/TextUtilss-React/static/css/main.a6a518b7.chunk.css"
+  },
+  {
+    "revision": "e38d0cf372aea3d9fb71",
+    "url": "/TextUtilss-React/static/js/main.e38d0cf3.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextUtilss-React/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TextUtilss-React/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "983833dae96172446e8c",
+    "url": "/TextUtilss-React/static/js/runtime~main.983833da.js"
+  },
+  {
+    "revision": "1de0c8c8486b1817c4b8dbe52dbce008",
+    "url": "/TextUtilss-React/index.html"
+  }
+];
